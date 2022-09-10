8b4f3e38f1a3f6f5f5d6eda2e07a9b681875cc89
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2022-09-10 21:16:02 +0200

    beans with default profile

--- src/main/java/guru/springframework/sfgdi/services/I18nGermanGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nGermanGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("DE")
+@Profile({"DE", "default"})
 @Service("i18nService")
 public class I18nGermanGreetingService implements GreetingService{
     @Override
